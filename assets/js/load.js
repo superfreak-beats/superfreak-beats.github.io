692c22a138e1d3828f77885cfcc394e502b301b7
superfreak-beats <superfreakbeats@gmail.com>
2025-04-07 12:16:51 +0900

    Add files via upload

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,25 +1,25 @@
 const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
 
 /* TEXT ANIMATION */
-tl.to(".text", {
+tl.add("start").to(".text", {
   y: "0%",
   duration: 1,
   stagger: 0.2,
-});
-tl.to(".headtext", {
+}, "start")
+.to(".headtext", {
   opacity: 1,
-  duration: .75,
-});
+  duration: 0.6,
+  delay: 0.875,
+}, "start");
 
 /* SLIDER OPTIONAL */
 // tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", {
   opacity: 0,
-  duration: .75,
+  duration: 0.75,
   delay: 0.35,
   display: "none",
 }); /* ,  */
 
 /* HOME PAGE CONTENT ANIMATION */
 tl.fromTo("main", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-
